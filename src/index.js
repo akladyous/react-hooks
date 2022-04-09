f49a75d9e5c2cf04153fa76f1b6c68046b46a9a6
Boula Akladyous <akladyous@gmail.com>
2022-04-09 13:59:58 -0400

    react-18 createRoot index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+//     document.getElementById("root")
+// );
+
+import App from "./App";
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+root.render(<App tab="home" />);
